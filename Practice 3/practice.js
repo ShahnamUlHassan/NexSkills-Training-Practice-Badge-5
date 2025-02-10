1f00dc42197c0e090d60774b6023453cf35d8f16
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-10 18:23:53 +0500

    Add files via upload

---
+++ Practice 3/practice.js
@@ -0,0 +1,13 @@
+alert("Hello World!");
+console.log("Hello World!");
+document.write("Hello World!");
+
+// data types
+// string
+document.write("Hello!")
+// number
+document.write(123);
+// int = positive and negative number even include . decimals
+// uint = positive numbers only
+name = prompt("Please enter your name!");
+alert(name);
