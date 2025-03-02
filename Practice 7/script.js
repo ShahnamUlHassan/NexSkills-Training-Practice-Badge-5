7abbe3e24e352351a0f21d3a02489c841aa02494
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-03-02 11:36:18 +0500

    Add files via upload

---
+++ Practice 7/script.js
@@ -0,0 +1,157 @@
+// var arr = ["Shahnam" , 55 , true]
+// console.log(arr);
+
+// var arr1 ;
+// console.log(arr1);
+
+// var arr2 =["Shahnam", "Hassan",["Allah",7],9];
+// console.log(arr2);
+// console.log(arr2[2]);
+
+// var c = [1,2,3];
+// var mm =[4,5,6];
+// var kk = [...c,...mm];
+// console.log(kk);
+
+
+// var d = [1,2,3];
+// var nn =[4,5,6];
+// var ll = [d,nn];
+// console.log(ll);
+
+// var obj = {
+//     name : "Shahnam",
+//     salary : 55000,
+//     age : 26
+// }
+// console.log(obj);
+// console.log(obj.name);
+
+// function abc(){
+//     return Math.floor(Math.random()*100000);
+// }
+// console.log(abc);
+
+// var a = prompt("Enter your name")
+// var users = [];
+// var newUsers = {
+//     id:abc,
+//     name:a,
+//     lastname:"Hassan"
+// }
+// users.push(newUsers)
+// users;
+
+// //mandeepkaur array method;
+
+
+// //Class Work
+// var array = ["Shahnam", 9 , "Hassan", 5];
+// console.log(array);
+// console.log(array[0]);
+
+// //2D Array or Multi-dimensional array method
+// var array1 = ["Shahnam", 9 , ["Hello", "Hassan"],6];
+// console.log(array1[2]);
+
+// var array2 = ["SHahnam", 9 , "Hassan", 8];
+// array2[0] = "Hello!";
+// console.log(array2);
+
+// var array3 = ["Shahnam", 9 , "Hassan", 8];
+// var [name,value,hello] = array3;
+// console.log(array3);
+
+// var array4 = ["Shahnam", 9 , "Hassan", 8];
+// var array5 = ["Hello!",7];
+// var res = [...array4,...array5];
+// console.log(res);
+
+// var array6 = ["Shahnam", 9 , "Hassan", 8];
+// var array7 = ["Hello!",7];
+// var result = [array6,array7];
+// console.log(result);
+
+// var object = {
+//     name : "Shahnam",
+//     age : 26,
+//     salary : 55000
+// }
+// console.log(object);
+// console.log(object.name);
+
+// var array8 = ["Shahnam", "Hello!", "Hassan"];
+// var array9 = prompt("Please enter your name");
+// array8.push(array9);
+// console.log(array8);
+
+
+// var array10 = ["Shahnam", "Hello!", "Hassan"];
+// var array11 = prompt("Please enter your name");
+// array8.pop(array11);
+// console.log(array10);
+
+
+// var array12 = ["Shahnam", "Hello!", "Hassan"];
+// var array13 = prompt("Please enter your name");
+// array8.shift(array13);
+// console.log(array12);
+
+
+// var array14 = ["Shahnam", "Hello!", "Hassan"];
+// var array15 = prompt("Please enter your name");
+// array8.unshift(array15);
+// console.log(array14);
+
+// var array16 = [1,9,2,8,3,7];
+// array16.reverse();
+// console.log(array16);
+
+// var array17 = ["z", "a", "y", "b"];
+// array17.sort();
+// console.log(array17);
+
+// function name (){
+//     Math.random();
+// }
+// console.log(name);
+
+// function name (){
+
+// }
+// Math.floor(Math.random()*10000);
+// console.log(name);
+
+// function name (){
+//     return Math.floor(Math.random()*10000);
+// }
+// console.log(name);
+
+// function name (){
+//     return Math.floor(Math.random()*10000)
+// }
+// var a = prompt("enter your name");
+// var users = [];
+// var newUsers = {
+//     id:abc(),
+//     name:a,
+//     lastName:"Hassan"
+// }
+// users.push(newUsers);
+// console.log(users);
+
+// function app (){
+//     function name (){
+//         return Math.floor(Math.random()*10000)
+//     }
+//     var a = prompt("enter your name");
+//     var users = [];
+//     var newUsers = {
+//         id:abc(),
+//         name:a,
+//         lastName:"Hassan"
+//     }
+//     users.push(newUsers);
+//     console.log(users);
+// }
+// app();
---
+++ Practice 8/script.js
@@ -0,0 +1,11 @@
+// let table = prompt("Enter your table number")
+// for (let i = 0; i <= 10; i++){
+//     console.log(`${table} * ${i} = ${table*i}`)
+// }
+
+let table = prompt("Enter your table number")
+let i = 0;
+while(i <= 10){
+    console.log(`${table} * ${i} = ${table*i}`)
+    i++;
+};
